b2f48d662e690c20a28d38eea6db6a447befebaa
Joshua Estrada <student.joshuaestrada@gmail.com>
2026-04-25 13:42:25 -0700

    add missing line

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,7 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("Connected to mongo"))
     .catch((err) => console.error('db connection error'))
+
+app.listen(3001, () => {
+    console.log("backend running on port 3001")
+})
