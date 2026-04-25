25f982c2912079bccd8e70952198fc286dc9fa0c
Joshua Estrada <student.joshuaestrada@gmail.com>
2026-04-25 13:41:39 -0700

    setup mongo connection

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const app = express()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to mongo"))
+    .catch((err) => console.error('db connection error'))
