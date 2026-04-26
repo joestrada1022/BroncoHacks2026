d0c566c1ad01e1bc0e85c38dc48c04e46c5153b8
Max Gross <maxgross72@gmail.com>
2026-04-26 09:38:16 -0700

    callbacks work properly

--- firmware/crop_recon_platformio/src/transmitter.cpp
+++ firmware/crop_recon_platformio/src/transmitter.cpp
@@ -2,16 +2,19 @@
 // #include <LoRa.h>
 
 // int counter = 0;
+
 // const int csPin = 7;          // LoRa radio chip select
 // const int resetPin = 0;        // LoRa radio reset
 // const int irqPin = 1;          // change for your board; must be a hardware interrupt pin
 
+// void onTxDone();
+// boolean runEvery(unsigned long interval);
+
 // void setup() {
 //   Serial.begin(9600);
 //   while (!Serial);
-//   sleep(1);
 
-//   Serial.println("LoRa Sender");
+//   Serial.println("LoRa Sender non-blocking Callback");
 
 //   LoRa.setPins(csPin, resetPin, irqPin);
 
@@ -19,20 +22,38 @@
 //     Serial.println("Starting LoRa failed!");
 //     while (1);
 //   }
-//   LoRa.dumpRegisters(Serial);
+
+//   LoRa.onTxDone(onTxDone);
 // }
 
 // void loop() {
-//   Serial.print("Sending packet: ");
-//   Serial.println(counter);
+//   if (runEvery(5000)) { // repeat every 5000 millis
 
-//   // send packet
-//   LoRa.beginPacket();
-//   LoRa.print("hello ");
-//   LoRa.print(counter);
-//   LoRa.endPacket();
+//     Serial.print("Sending packet non-blocking: ");
+//     Serial.println(counter);
 
-//   counter++;
+//     // send in async / non-blocking mode
+//     LoRa.beginPacket();
+//     LoRa.print("hello ");
+//     LoRa.print(counter);
+//     LoRa.endPacket(true); // true = async / non-blocking mode
 
-//   delay(5000);
+//     counter++;
+//   }
+// }
+
+// void onTxDone() {
+//   Serial.println("TxDone");
+// }
+
+// boolean runEvery(unsigned long interval)
+// {
+//   static unsigned long previousMillis = 0;
+//   unsigned long currentMillis = millis();
+//   if (currentMillis - previousMillis >= interval)
+//   {
+//     previousMillis = currentMillis;
+//     return true;
+//   }
+//   return false;
 // }
